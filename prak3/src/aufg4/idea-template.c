6ba6de510336cebef863e95f7e0a1f5e1b489c8b
Eric <s67766@informatik.htw-dresden.de>
2011-11-21 21:50:54 +0100

    prak3 fertig

--- prak3/src/aufg4/idea-template.c
+++ prak3/src/aufg4/idea-template.c
@@ -8,53 +8,75 @@ unsigned char input[512];    /* for encrypted text */
 unsigned char output[512];   /* for decrypted text */
 
 unsigned char key[16] = {           /* 128 bit key */
-    0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
-    0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+  0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+  0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
 
 static unsigned char iv[8] = {  /* 64 bit IV block */
-    0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef};
+  0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef};
 
 int main(void)
 {
-	int i, n, t1, t2, t3, t4;
-	long length;
-	IDEA_KEY_SCHEDULE encrypt_ks, decrypt_ks;
-	unsigned char tmp[8];
+  int i, n, t1, t2, t3, t4, t5;
+  long length;
+  IDEA_KEY_SCHEDULE encrypt_ks, decrypt_ks;
+  unsigned char tmp[8];
 
-	/* open the given text files */
-	t1 = open("text1.bin", O_RDONLY);
-	t2 = open("text2.bin", O_RDONLY);
-	t3 = open("text3.bin", O_RDONLY);
-	t4 = open("text4.bin", O_RDONLY);
-	length = 512;
+  /* open the given text files */
+  t1 = open("text1.bin", O_RDONLY);
+  t2 = open("text2.bin", O_RDONLY);
+  t3 = open("text3.bin", O_RDONLY);
+  t4 = open("text4.bin", O_RDONLY);
+  //t5 = open("zusatz.bin", O_RDONLY);
+  length = 512;
 
-	/* read 512 bytes from file t4 */
-	read(t4, input, length);
-	/* ... decrypt input in ECB mode */
-	idea_set_encrypt_key(key, &encrypt_ks);
-	idea_set_decrypt_key(&encrypt_ks,&decrypt_ks);
-        for(i=0; i<length; i+=8)
-	{
-		idea_ecb_encrypt(&input[i],&output[i],&decrypt_ks);
-	}
-	/* show decrypted text on stdout */
-	printf("%s\n", output);
+  /* read 512 bytes from file t4 */
+  read(t4, input, length);
+  /* ... decrypt input in ECB mode */
+  idea_set_encrypt_key(key, &encrypt_ks);
+  idea_set_decrypt_key(&encrypt_ks,&decrypt_ks);
+  for(i=0; i<length; i+=8)
+  {
+    idea_ecb_encrypt(&input[i],&output[i],&decrypt_ks);
+  }
+  /* show decrypted text on stdout */
+  printf("%s\n", output);
 
-	read(t1, input, length);
-	/* ... decrypt input in CBC mode */
-        for(i=0; i<length; i+=8)
-	{
-		idea_cbc_encrypt(&input[i],&output[i],8,&decrypt_ks,iv, IDEA_DECRYPT);
-	}
-	printf("%s\n", output);
+  read(t1, input, length);
+  /* ... decrypt input in CBC mode */
+  memcpy(tmp, iv, 8);
+  for(i=0; i<length; i+=8)
+  {
+    idea_cbc_encrypt(&input[i],&output[i],8,&decrypt_ks, tmp, IDEA_DECRYPT);
+  }
+  printf("%s\n", output);
 
-	read(t2, input, length);
-	/* ... decrypt input in CFB mode */
-	printf("%s\n", output);
+  read(t2, input, length);
+  /* ... decrypt input in CFB mode */
+  i=0;
+  memcpy(tmp, iv, 8);
+  idea_cfb64_encrypt(input, output, length, &encrypt_ks, tmp, &i, IDEA_DECRYPT);
 
-	read(t3, input, length);
-	/* ... decrypt input in OFB mode */
-	printf("%s\n", output);
+  printf("%s\n", output);
 
-	return 0;
+  read(t3, input, length);
+  /* ... decrypt input in OFB mode */
+  memcpy(tmp, iv, 8);
+  idea_ofb64_encrypt(input, output, length, &encrypt_ks, tmp, &i);
+  printf("%s\n", output);
+
+  /* read 512 bytes from file t5 */
+  /*read(t5, input, length);
+  for(i=0; i<length; i+=8)
+  {
+    idea_ecb_encrypt(&input[i],&output[i],&decrypt_ks);
+  }*/
+  /* show decrypted text on stdout */
+  //printf("%s\n", output);
+
+  //close(t5);
+  close(t4);
+  close(t3);
+  close(t2);
+  close(t1);
+  return 0;
 }
